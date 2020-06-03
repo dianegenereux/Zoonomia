575cb7a24670e896fac6c239764f9864e15788ba
dianegenereux <dianegenereux@gmail.com>
2020-06-03 11:36:16 -0400

    Create IUCN.diversity.correlation.R

---
+++ IUCN.diversity.correlation.R
@@ -0,0 +1,40 @@
+z=read.table("diversity_analysis.input.txt", header=T)
+
+library(ordPens)
+#1. pop
+#2. IUCN
+#3. het 
+#4. soh 
+
+###MATRIX PREP
+for.het<-data.frame(as.numeric(z[,9]), as.factor(as.integer(z[,2])),as.factor(as.integer(z[,3])))
+for.soh<-data.frame(as.numeric(z[,10]), as.factor(as.integer(z[,2])),as.factor(as.integer(z[,3])))
+
+
+#het and soh can't be NA, so remove any species missing value for the relevant analysis
+for.het<-for.het[!is.na(for.het[, 9]), ]
+for.soh<-for.soh[!is.na(for.soh[, 10]), ]
+
+#exclude species for which IUCN is missing
+for.het.iucn<-for.het[!is.na(for.het[, 2]), ]
+for.soh.iucn<-for.soh[!is.na(for.soh[, 2]), ]
+
+
+#infer relationship between IUCN status and het, soh | these data
+het.iucn.coeff<- ordSmooth(as.numeric(for.het.iucn[,2]), for.het.iucn[,1], offset = rep(0,length(for.het.iucn[,2])), lambda=1000, model = c("linear"))$coef
+soh.iucn.coeff<-ordSmooth(as.numeric(for.soh.iucn[,2]), for.soh.iucn[,1], offset = rep(0,length(for.soh.iucn[,2])), lambda=1000, model = c("linear"))$coef
+
+####determine relationship if labels assigned at random, determine probability of real observation
+real.iucn<-for.het.iucn[,2]
+it<-10000
+reorder.iucn<-matrix(0, length(real.iucn),it)
+randomize.iucn<- for (i in 1:it)
+
+{
+	#print(i)
+	random.iucn<-sample(real.iucn)
+	#print(random.iucn)
+#reorder.iucn[,i]<-random.iucn
+test.coeff<-ordSmooth(as.numeric(random.iucn), for.het.iucn[,1], offset = rep(0,length(for.het.iucn[,2])), lambda=1000, model = c("linear"))$coef
+reorder.iucn[,i]<-mean(test.coeff[3:6])
+}
